06bf995fd99726de4bac7de42fb171527ffdd28c
cuiqs <cuiqs@knownsec.com>
2017-12-27 23:54:28 +0800

    login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -2,6 +2,7 @@
  * @summary 登录页面
  */
 import React, { Component } from 'react'
+import { Button } from 'antd'
 
 import logo from './images/logo.svg'
 import style from './style.css'
@@ -21,6 +22,26 @@ class Login extends Component {
             <img src={logo} alt="" />
             <div className={style.title}>云镜</div>
             <p>用心做好镜</p>
+          </div>
+          
+          <div className={style['bottom-box']}>
+            <div className={style['input-box']}>
+              <input type="text" />
+              <button>发送验证码</button>
+            </div>
+
+            <div className={style['verification-box']}>
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+            </div>
+
+            <div className={style['btn-box']}><Button type="primary">提交</Button></div>
+
+            <div className={style['tip-box']}>电话不可用？<a>其他登录方式</a></div>
           </div>
         </div>
       </div>
