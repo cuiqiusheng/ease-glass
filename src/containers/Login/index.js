09dc8f44ce68e09bbc3624a84fd9726ec19daf17
cuiqs <cuiqs@knownsec.com>
2017-12-28 16:34:41 +0800

    login with phone

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -3,6 +3,7 @@
  */
 import React, { Component } from 'react'
 import { Button } from 'antd'
+import classnames from 'classnames'
 
 import logo from './images/logo.svg'
 import style from './style.css'
@@ -12,6 +13,25 @@ class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+  }
+
+  onVrfChange = (e) => {
+    const { value, className } = e.target
+    const reg = new RegExp('^[0-9]*$')
+    if (value) {
+      if (value.length > 1) {
+        e.target.value = value.charAt(0)
+      }
+      if (reg.test(value)) {
+        const index = +className.charAt(className.length - 1)
+        if (index < 6) {
+          document.querySelector(`.vrf-${index + 1}`).focus()
+        }
+      } else {
+        e.target.value = ''
+      }
+    }
+    e.target.value.replace(/[^\d]/g, '')
   }
 
   render() {
@@ -25,18 +45,21 @@ class Login extends Component {
           </div>
           
           <div className={style['bottom-box']}>
-            <div className={style['input-box']}>
-              <input type="text" />
-              <button>发送验证码</button>
+            <div className={classnames('clearfix', style['input-box'])}>
+              <div className="fl">
+                <div>电话号码</div>
+                <input type="text" />
+              </div>
+              <button className="fl">发送验证码</button>
             </div>
 
             <div className={style['verification-box']}>
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
+              <input type="text" className="vrf-1" key="1" onChange={this.onVrfChange} />
+              <input type="text" className="vrf-2" key="2" onChange={this.onVrfChange} />
+              <input type="text" className="vrf-3" key="3" onChange={this.onVrfChange} />
+              <input type="text" className="vrf-4" key="4" onChange={this.onVrfChange} />
+              <input type="text" className="vrf-5" key="5" onChange={this.onVrfChange} />
+              <input type="text" className="vrf-6" key="6" onChange={this.onVrfChange} />
             </div>
 
             <div className={style['btn-box']}><Button type="primary">提交</Button></div>
